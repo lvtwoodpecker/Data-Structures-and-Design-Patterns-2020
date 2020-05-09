df70c6fc03677f9bb7e472ac15f02f5da47530e6
lvtwoodpecker <51301215+lvtwoodpecker@users.noreply.github.com>
2020-05-09 04:32:01 -0400

    Update main.swift

--- week6/Red Black Tree/main.swift
+++ week6/Red Black Tree/main.swift
@@ -1,5 +1,11 @@
-//Testing testing testing testing
+//
+//  main.swift
+//  Cedric - Red Black Tree
+//  This is an exercise for the Data Structures class. Implement a Red Black Tree, which is a binary search tree but optimized?
+//  Created by Cedric on 4/11/20.
+//  Copyright © 2020 Cedric Lam. All rights reserved.
 
+// Driver file
 let tree = RedBlackTree<Int>()
 tree.insert(element: 4)
 tree.insert(element: 7)
@@ -19,4 +25,33 @@ print("Height:", tree.height)
 print("Ordered:", tree.elements)
 print("Size:", tree.size)
 print(tree.contains(element: 2))
-print(tree.search(element: 103))
+
+tree.delete(element: 4)
+print("After deleting 4", tree.makeBreadthFirstArray())
+print("Ordered:", tree.elements)
+print("Size:", tree.size)
+print("Height:", tree.height)
+print(tree.checkBalanced())
+
+
+let tree2 = RedBlackTree<Int>(fromSortedData : [1,2,3,4,5,6,7,8,9,10])
+print("Tree 2:", tree2.makeBreadthFirstArray())
+print("Ordered:", tree2.elements)
+print("Size:", tree2.size)
+print("Height:", tree2.height)
+print(tree2.checkBalanced())
+
+tree2.insert(element: 100)
+tree2.insert(element: 1100)
+tree2.insert(element: 1200)
+
+print("Tree 2:", tree2.makeBreadthFirstArray())
+print(tree2.checkBalanced())
+
+tree2.delete(element:1200)
+print("Tree 2:", tree2.makeBreadthFirstArray())
+print("After deleting 1200", tree2.makeBreadthFirstArray())
+print("Ordered:", tree2.elements)
+print("Size:", tree2.size)
+print("Height:", tree2.height)
+print(tree2.checkBalanced())
