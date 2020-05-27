31c1891dba3220f2ddcd9d6dad408828f559bc99
Thang Lam <51301215+lvtwoodpecker@users.noreply.github.com>
2020-05-27 11:26:56 -0400

    Update main.swift

--- week9/project files/main.swift
+++ week9/project files/main.swift
@@ -3,17 +3,18 @@
 let graph = Graph<Int>(vertices: 5)
 graph.addEdge(firstVertex: 1, secondVertex: 2)
 graph.addEdge(firstVertex: 0, secondVertex: 1)
-graph.addEdge(firstVertex: 3, secondVertex: 0)
+//graph.addEdge(firstVertex: 3, secondVertex: 0)
 graph.addEdge(firstVertex: 4, secondVertex: 2)
 // graph.addEdge(firstVertex: 4, secondVertex: 1)
 // graph.addEdge(firstVertex: 2, secondVertex: 0)
-graph.addEdge(firstVertex: 3, secondVertex: 4)
+//graph.addEdge(firstVertex: 3, secondVertex: 4)
 
 
 print(graph.vertexArray)
 
 print(graph.neighborsOf( vertex : 1))
-print(graph.neighborsOf( vertex : 2))
+
+print(graph.verticesAreConnected(firstVertex : 2, secondVertex : 4))
 
 print(graph.verticesAreAdjacent(firstVertex: 2, secondVertex: 1))
 
